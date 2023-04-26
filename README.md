3127806eab00037dddc889eb00d28c648371fdbf
Sudeep <sudeeptiwari12345@gmail.com>
2023-04-26 23:16:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# *ProjectInfo*
+# ***💳 Credit Card Default Prediction***
+This is a binary classification problem.In this we have to predict whether the user of credit card will default on the payment in next month or not.
+An api using streamlit has been created,with the help of the api we can take input data and do the prediction.
+
+For information about dataset and project check the jupyter file and html file present in the notebook folder in the github repository of this project.
+
+# **API**
+
+
+
+# **Installation guide**
+
+Step1:Install the setup.py file:
+pip install .
+
+While installing the setup.py file the current working directory
+should be the same as that setup.py file.
+
+Step2: Run the streamlit app:
+streamlit run app.py
+
+
+
+
+# ***How to do the Predictions using API***
+
+Step1: Go to the following url:
+
+Step2: Insert the file at the required location
+
+Step3: Wait for the prediction.
+
+For making it easier for testing the model I have provided
+some test files in the github repository of this
+project.You can download one of those csv files and
+can use them for testing the app.
+
---
+++ app.py
@@ -0,0 +1,59 @@
+from CreditCardDefault.pipeline.batch_prediction import start_batch_prediction
+from CreditCardDefault.pipeline.training_pipeline import TrainPipeline
+from CreditCardDefault.logger import logging
+from CreditCardDefault.exception import CreditCardDefaultException
+import os,sys
+
+import streamlit as st
+import pandas as pd
+
+st.header("Credit Card Default Prediction Application")
+
+st.subheader("Please upload the input csv file:")
+input_file = st.file_uploader("Upload the file")
+
+print(__name__)
+logging.info(f"Running the training pipeline")
+train_pipeline = TrainPipeline()
+train_pipeline.run_pipeline()
+
+logging.info("getting the file paths of the required objects")
+transformer_object_path = train_pipeline.data_transformation_artifact.transformer_object_path
+model_path = train_pipeline.model_train_eval_artifact.model_path
+f1_train_score = train_pipeline.model_train_eval_artifact.f1_train_score
+f1_test_score = train_pipeline.model_train_eval_artifact.f1_test_score
+
+
+if __name__=="__main__":
+
+     try:
+        if input_file:
+               logging.info("Starting the prediction")
+               output_file_path = start_batch_prediction(input_file,transformer_object_path,model_path)
+               df = pd.read_csv(output_file_path)
+               prediction_column = df["Prediction"]
+               actual_output = df["actual_output"]
+               dict_replace = {
+                    1 : "Defaulter",
+                    0: "Not a Defaulter"
+                    }
+               prediction_in_words = prediction_column.replace(dict_replace)
+               actual_output_in_words = actual_output.replace(dict_replace)
+               output_df = pd.DataFrame([prediction_in_words,actual_output_in_words])
+               logging.info("Displaying the prediction result in webpage")
+               st.subheader("Result")
+               st.dataframe(output_df)
+               st.subheader("F1 Score of the model")
+               score_dict = {"Train Score" : f1_train_score,
+                             "Test Score" : f1_test_score}
+
+               st.dataframe(score_dict)
+
+     except Exception as e:
+          raise CreditCardDefaultException(e,sys)
+
+
+
+
+
+
